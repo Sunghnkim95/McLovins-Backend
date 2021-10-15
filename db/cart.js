3a824e6b6c555fa2f8ecd2437003a7343a3436a3
Your Name <you@example.com>
2021-10-14 21:53:13 -0400

    more helper function checks

--- db/cart.js
+++ db/cart.js
@@ -1,13 +1,13 @@
 const client  = require('./client');
 const {getProductById} = require('./products')
 
-async function createCart () {
+async function createCart ({userId, active}) {
     try{
         const { rows: [ cart ] } = await client.query(`
-        INSERT INTO cart ("userId")
-        VALUES ($1)
+        INSERT INTO cart ("userId", active)
+        VALUES ($1, $2)
         RETURNING *;
-    `, [userId]); 
+    `, [userId, active]); 
     return cart;
     }catch (error){
         throw error;
@@ -69,31 +69,3 @@ module.exports = {
 
     
   }
-    // CREATE TABLE product (
-    //     id SERIAL PRIMARY KEY,
-    //     name VARCHAR(255) UNIQUE NOT NULL,
-    //     description VARCHAR(255) NOT NULL,
-    //     category VARCHAR(255) NOT NULL,
-    //     quantity INTEGER NOT NULL,
-    //     price DECIMAL(10,2) NOT NULL,
-    //     photo VARCHAR(255) NOT NULL
-    //     );
-
-
-    //  CREATE TABLE cart_item (
-    //     id SERIAL PRIMARY KEY,
-    //     product_id INTEGER REFERENCES product(id),
-    //     item_quantity INTEGER NOT NULL,
-    //     price DECIMAL(10,2) NOT NULL
-    //   );
-
-    
-    //   CREATE TABLE cart (
-    //     id SERIAL PRIMARY KEY,
-    //     "userId" INTEGER REFERENCES users(id),
-    //     "itemId" INTEGER REFERENCES cart_item(id),
-            
-    //     active BOOLEAN DEFAULT TRUE
-    //     );
-    
-
