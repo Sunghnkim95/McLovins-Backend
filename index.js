90566f33227ac29a1bbb1bd25988b999d29ff707
John <jvt703@gmail.com>
2021-10-16 13:39:53 -0400

    removed some of the extra stuff jay addded for testing

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env
 const express = require('express');
 const server = express();
-const helmet = require('helmet');
 const router = express.Router()
-//server.use(helmet());
+
 const bodyParser = require('body-parser');
 server.use(bodyParser.json());
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+
 server.use(function(req, res, next) {
     res.setHeader('Content-Security-Policy', "connect-src 'self' localhost")
     return next()
@@ -25,8 +21,6 @@ server.use('/api', apiRouter);
 const client = require('./db/client');
 
 const morgan = require('morgan');
-const { appendFile } = require('fs');
-const { getOrderHistoryByUserId } = require('./db');
 server.use(morgan('dev'));
 
 server.use((req, res, next) =>{
