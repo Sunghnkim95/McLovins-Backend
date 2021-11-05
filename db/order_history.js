e7fdacce3d7972c22ca72fb7fee06662c8921294
peter <sunghnkim95@gmail.com>
2021-11-05 00:59:08 -0400

    testing

--- db/order_history.js
+++ db/order_history.js
@@ -4,10 +4,10 @@ async function createOrderHistory ({userId, cartId, fullname , email, address, c
     console.log('createOrderHistory props', userId, cartId, fullname , email, address, city, state, zip, cardname, cardnumber, expmonth, expyear, cvv);
     try{
         const { rows: [ orders ] } = await client.query(`
-        INSERT INTO order_history ("userId", "cartId", "fullname" , "email", "address", "city", "state", "zip", "cardname", "cardnumber", "expmonth", "expyear", "cvv")
+        INSERT INTO order_history ("userId", "cartId", "fullname" , "email", "street", "city", "state", "zip", "cardname", "cardnumber", "expmonth", "expyear", "cvv")
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
         RETURNING *;
-    `, [userId, cartId, fullname , email, address, city, state, zip, cardname, cardnumber, expmonth, expyear, cvv]); 
+    `, [userId, cartId, fullname , email, street, city, state, zip, cardname, cardnumber, expmonth, expyear, cvv]); 
     console.log('new orders', orders);
     return orders;
     }catch (error){
