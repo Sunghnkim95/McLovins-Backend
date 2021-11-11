339a16769ec4cf7a64a9201577a33ffe366119c7
peter <sunghnkim95@gmail.com>
2021-11-10 19:41:01 -0500

    set

--- db/products.js
+++ db/products.js
@@ -89,6 +89,8 @@ async function updateProduct (fields) {
 }
 async function deleteProduct(id){
     console.log('THIS THAT DB PRODUCT ID', rows);
+    console.log('THIS THAT DB PRODUCT ID253', id);
+
     try{
        const {rows: [product]} = await client.query(`
            DELETE FROM product
